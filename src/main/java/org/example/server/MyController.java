b8bf6e419080db670bd6adcd86e7341955a45240
kimcider <rlaqudcjs96@naver.com>
2024-10-13 03:02:20 +0900

    새로 입장하면 기존의 유저들에게 상태정보 받아오는 기능 추가. 테스트코드 만들어야함

--- src/main/java/org/example/server/MyController.java
+++ src/main/java/org/example/server/MyController.java
@@ -1,8 +1,10 @@
 package org.example.server;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Getter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,14 +14,30 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 @Getter
 public class MyController {
-    private ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    public static String wrapMethod(String method, String json) {
+        ObjectNode wrappedData = mapper.createObjectNode();
+        try{
+            wrappedData.put("method", method);
+
+            if(!json.isBlank()){
+                JsonNode jsonNode = mapper.readTree(json);
+                wrappedData.set("data", jsonNode);
+            }
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return wrappedData.toString();
+    }
 
     @PostMapping("/sendLinerStatus")
     public void sendLinerStatus(@RequestBody String json) {
@@ -29,15 +47,17 @@ public class MyController {
 
             String hash = rootNode.get("hash").asText();
             JsonNode dataNode = rootNode.get("data");
+            //이게 null일 일이 뭐가있지?
             if (dataNode == null) {
                 return;
             }
             String dataJson = mapper.writeValueAsString(dataNode);
+            String wrappedJson = wrapMethod("sendLinerStatus", dataJson);
 
             MyWebSocketHandler myWebSocketHandler = MyWebSocketHandler.getInstance();
             myWebSocketHandler.sessionMap.get(hash).stream().filter(WebSocketSession::isOpen).forEach(session -> {
                 try {
-                    session.sendMessage(new TextMessage(dataJson));
+                    session.sendMessage(new TextMessage(wrappedJson));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -47,4 +67,25 @@ public class MyController {
         }
     }
 
+//    //얘를 여기다 두지 말고 handleTextMessage 에 넣어야할듯
+//    @PostMapping("/getLinerStatus")
+//    public String getLinerStatus(@RequestBody String hashValue){
+//        MyWebSocketHandler.getInstance().sessionMap.get(hashValue).stream().filter(WebSocketSession::isOpen).forEach(session -> {
+//            try {
+//                session.sendMessage(new TextMessage(wrapMethod("getLinerStatus", "")));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//    }
+
+    @PostMapping("/test")
+    public String testTest(@RequestBody String json){
+        return """
+                [{"name":"top","flash":{"type":"flash","coolTime":60},"cosmicInsight":false,"ionianBoots":false},
+                {"name":"bot","flash":{"type":"flash","coolTime":0},"cosmicInsight":false,"ionianBoots":false},
+                {"name":"mid","flash":{"type":"flash","coolTime":0},"cosmicInsight":false,"ionianBoots":false},
+                {"name":"jg","flash":{"type":"flash","coolTime":0},"cosmicInsight":true,"ionianBoots":false},
+                {"name":"sup","flash":{"type":"flash","coolTime":0},"cosmicInsight":false,"ionianBoots":false}]""";
+    }
 }
--- src/main/java/org/example/server/MyWebSocketHandler.java
+++ src/main/java/org/example/server/MyWebSocketHandler.java
@@ -1,44 +1,96 @@
 package org.example.server;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 @Component
 public class MyWebSocketHandler extends TextWebSocketHandler {
+    private ObjectMapper mapper = new ObjectMapper();
     private static MyWebSocketHandler myWebSocketHandler = new MyWebSocketHandler();
+    public HashMap<String, Set<WebSocketSession>> sessionMap;
+    public HashMap<String, Set<WebSocketSession>> uninitializedSessionMap;
+    public HashMap<Integer, String> sessionHashValue;
+
 
     private MyWebSocketHandler(){
-
+        sessionMap = new HashMap<>();
+        sessionHashValue = new HashMap<>();
+        uninitializedSessionMap = new HashMap<>();
     }
 
     public static MyWebSocketHandler getInstance(){
         return myWebSocketHandler;
     }
 
-    public HashMap<String, Set<WebSocketSession>> sessionMap = new HashMap<>();
-    public HashMap<Integer, String> sessionHashValue = new HashMap<>();
-
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
 
     }
 
+    WebSocketSession unInitializedSession;
+
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {
-        String hashValue = message.getPayload();
-        sessionHashValue.put(session.hashCode(), hashValue);
+        try{
+            JsonNode rootNode = mapper.readTree(message.getPayload());
 
-        if(!sessionMap.containsKey(hashValue)){
-            sessionMap.put(hashValue, new HashSet<>());
+            String method = rootNode.get("method").asText();
+            String hash = rootNode.get("hash").asText();
+            System.out.println("method: " + method + "hash: " + hash);
+            if("open".equals(method)){
+                sessionHashValue.put(session.hashCode(), hash);
+
+                if(!sessionMap.containsKey(hash)){
+                    sessionMap.put(hash, new HashSet<>());
+                    sessionMap.get(hash).add(session);
+                }else{
+                    if(!uninitializedSessionMap.containsKey(hash)){
+                        uninitializedSessionMap.put(hash, new HashSet<>());
+                    }
+                    uninitializedSessionMap.get(hash).add(session);
+                }
+            }else if("getLinerStatus".equals(method)){
+                sessionMap.get(hash).stream().filter(WebSocketSession::isOpen).forEach(sses -> {
+                    try {
+                        sses.sendMessage(new TextMessage(MyController.wrapMethod("getLinerStatus", "")));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }else if("getLinerStatusResponse".equals(method)){
+                JsonNode dataNode = rootNode.get("data");
+                String dataJson = mapper.writeValueAsString(dataNode);
+                if(uninitializedSessionMap.size() == 0
+                        || uninitializedSessionMap.get(hash) == null
+                        || uninitializedSessionMap.get(hash).size() == 0){
+                    return;
+                }
+
+                uninitializedSessionMap.get(hash).stream().filter(WebSocketSession::isOpen).forEach(sess -> {
+                    try {
+                        sess.sendMessage(new TextMessage(MyController.wrapMethod("getLinerStatusResponse", dataJson)));
+                        sessionMap.get(hash).add(sess);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                uninitializedSessionMap.get(hash).clear();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        sessionMap.get(hashValue).add(session);
     }
 
     @Override
